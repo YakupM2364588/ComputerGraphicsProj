01dfb94ecd271635c4979b4de285b033fc9a2896
YakupM2364588 <yakup.marangoz@student.uhasselt.be>
2025-05-27 21:36:46 +0200

    Add files via upload

---
+++ Header Files/button.h
@@ -0,0 +1,49 @@
+//
+// Created by p0l on 5/27/25.
+//
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <string>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "shaderClass.h"
+
+class Button {
+public:
+    float x, y, width, height;
+    std::string text;
+    bool visible;
+    bool isPressed;
+
+    glm::vec3 colorID;
+
+    Button(float x = 50.0f, float y = 50.0f, float width = 150.0f, float height = 40.0f,
+           const std::string& text = "BUTTON", const glm::vec3& colorID = glm::vec3(1.0f, 1.0f, 1.0f));
+
+    ~Button();
+
+    void Initialize();
+
+    void Render(Shader& buttonShader, int windowWidth, int windowHeight);
+
+    void RenderForPicking(Shader& pickingShader, int windowWidth, int windowHeight);
+
+    bool HandleClick(double mouseX, double mouseY, int windowWidth, int windowHeight,
+                    GLuint pickingFBO, GLuint pickingTexture, Shader& pickingShader);
+
+    void SetColor(const glm::vec4& color);
+
+    void SetVisible(bool visible);
+
+private:
+    GLuint VAO, VBO;
+    bool initialized;
+    glm::vec4 currentColor;
+
+    glm::mat4 GetTransformMatrix(int windowWidth, int windowHeight) const;
+};
+
+#endif //BUTTON_H
